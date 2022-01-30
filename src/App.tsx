2aa73bbd0a59a1e5bc3d4c8dab52ef788ec3d9e7
Elias Zigaran <zigaraneliasnatan@gmail.com>
2022-01-30 20:05:35 -0300

    setting up eslint and prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App = (): JSX.Element => {
   return (
@@ -10,12 +10,7 @@ const App = (): JSX.Element => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
